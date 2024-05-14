bae5e36b6f0312802838916ac50d409612007a5b
SteffRhes <stefanresch@protonmail.com>
2024-05-15 00:41:28 +0200

    implement recursive udpipe call logic when a folder is passed instead of a file

--- src/main/infer.sh
+++ src/main/infer.sh
@@ -32,7 +32,95 @@ build_args() {
   echo "$result"
 }
 
+# function to call udpipe. If the parameters are files, udpipe is called directly on them. If 
+# the parameters are folders, then this function goes through the content of the folder recursively
+# and creating equivalent output folder / file structure for each matching txt file input.
+do_udpipe_recursively() {
+
+  # quick info on current recursive level / parameters
+  echo "do_udpipe_recursively" "$1" "$2"
+
+  # if file
+  if [ -f "$1" ]; then
+
+    # if txt
+    if [[ "$1" == *.txt ]]; then
+
+      # since the current parameter is a txt file, call udpipe on it
+      echo "is txt. processing"
+
+      # create parent folder of output (if it doesn't exist)
+      out_conllu_path_folder=${2%/*}
+      if ! [ -e "$out_conllu_path_folder" ]; then
+        mkdir -p "$out_conllu_path_folder"
+      fi
+
+      # call udpipe
+      command="udpipe $udpipe_args ${in_model_path} --outfile=${2} ${1}"
+      echo "constructed command: ${command}"
+      $command
+
+    else
+
+      # if the input parameter is a non-txt file, this must have been a mistake. Abort then.
+      echo "not a txt file" "$1"
+      exit 1
+    fi
+
+  # if folder
+  elif [ -d "$1" ]; then
+    echo "check content of folder" "$1"
+
+    # iterate over content of folder
+    for file_or_folder in "$1"/*; do
+      echo "check if file or folder" "$file_or_folder"
+
+      # if file
+      if [ -f "$file_or_folder" ]; then
+
+        # if txt
+        if [[ "$file_or_folder" == *.txt ]]; then
+          echo "is txt. creating equivalent target conllu"
+
+          # since this txt file was detected by iterating over a folder (potentially recursively), 
+          # an equivalent output folder / file structure must be created dynamically. 
+          in_txt_path_single=$file_or_folder
+
+          # remove /veld/input/* so that only relevant subfolder stays
+          out_conllu_path_single=${in_txt_path_single#$in_txt_path}
+
+          # replace .txt with .conllu
+          out_conllu_path_single=${out_conllu_path_single/.txt/.conllu}
+
+          # concatenate main output folder passed via environment with dynamically created subpath
+          out_conllu_path_single="${out_conllu_path}${out_conllu_path_single}"
+
+          # step further down, to reuse the udpipe creation logic at the beginning of this
+          # function
+          do_udpipe_recursively "$in_txt_path_single" "$out_conllu_path_single"
+        else
+
+          # is file, but not txt. Ignore then.
+          echo "is neither folder, nor txt"
+        fi 
+
+      # if folder
+      elif [ -d "$file_or_folder" ]; then
+        echo "is folder"
+
+        # recurse downwards into this subfolder
+        do_udpipe_recursively "$file_or_folder"
+      fi
+    done
+  else
+
+    # if parameter is a non-existing path
+    echo "parameter is neither file, nor path. Exiting."
+    exit 1
+  fi
+}
+
 udpipe_args=$(build_args)
-command="udpipe $udpipe_args ${model_path} --outfile=${output_conllu_file_path} ${input_text_file_path}"
-echo "constructed command: ${command}"
-$command
+
+do_udpipe_recursively "$in_txt_path" "$out_conllu_path"
+
