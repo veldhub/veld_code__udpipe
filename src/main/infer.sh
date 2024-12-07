9d5c71f3dcb624b30b9017cc44c9852807b2e60c
SteffRhes <steffrhes@pm.me>
2024-12-07 21:24:16 +0100

    remove url handling; simplify recursive processing; adapt internal folder handling

--- src/main/infer.sh
+++ src/main/infer.sh
@@ -2,26 +2,6 @@
 
 source ./read_input_options.sh
 
-if [[ "$in_txt_url" != "" ]]; then
-  if [[ "$in_txt_path" != "" ]]; then
-    echo "both in_txt_url and in_txt_path are provided. Exiting. Define only one." 
-    exit 1
-  fi
-  echo "downloading txt from ${in_txt_url}"
-  curl -o /tmp/tmp.txt "$in_txt_url"
-  in_txt_path=/tmp/tmp.txt
-fi
-
-if [[ "$in_model_url" != "" ]]; then
-  if [[ "$in_model_path" != "" ]]; then
-    echo "both in_model_url and in_model_path are provided. Exiting. Define only one." 
-    exit 1
-  fi
-  echo "downloading model from ${in_model_url}"
-  curl -o /tmp/tmp.udpipe "$in_model_url"
-  in_model_path=/tmp/tmp.udpipe
-fi
-
 build_args() {
   result=""
 
@@ -57,8 +37,8 @@ build_args() {
 # and creating equivalent output folder / file structure for each matching txt file input.
 do_udpipe_recursively() {
 
-  # quick info on current recursive level / parameters
-  # echo "do_udpipe_recursively" "$1" "$2"
+  # debug echo: on current recursive level / parameters
+  echo "apply udpipe recursively on" "$1" "$2"
 
   # if file
   if [ -f "$1" ]; then
@@ -66,81 +46,43 @@ do_udpipe_recursively() {
     # if txt
     if [[ "$1" == *.txt ]]; then
 
-      # since the current parameter is a txt file, call udpipe on it
-      # echo "is txt. processing"
-
-      # create parent folder of output (if it doesn't exist)
-      out_conllu_path_folder=${2%/*}
+      # prepare output conllu folder and file
+      out_conllu_file=${1/\/veld\/input\/txt\//\/veld\/output\/}
+      out_conllu_file=${out_conllu_file%txt}conllu
+      out_conllu_path_folder=${out_conllu_file%/*}
       if ! [ -e "$out_conllu_path_folder" ]; then
         mkdir -p "$out_conllu_path_folder"
       fi
 
       # call udpipe
-      command="udpipe $udpipe_args ${in_model_path} --outfile=${2} ${1}"
-      echo "executing: ${command}"
-      $command
+      command="udpipe $udpipe_args /veld/input/model/${in_model_file} --outfile="$out_conllu_file" ${1}"
+      echo "is txt. executing:"
+      echo "$command"
+      eval "$command"
 
-    else
-
-      # if the input parameter is a non-txt file, this must have been a mistake. Abort then.
-      # echo "not a txt file" "$1"
-      exit 1
     fi
 
   # if folder
   elif [ -d "$1" ]; then
-    # echo "check content of folder" "$1"
 
     # iterate over content of folder
     for file_or_folder in "$1"/*; do
-      # echo "check if file or folder" "$file_or_folder"
-
-      # if file
-      if [ -f "$file_or_folder" ]; then
-
-        # if txt
-        if [[ "$file_or_folder" == *.txt ]]; then
-          # echo "is txt. creating equivalent target conllu"
-
-          # since this txt file was detected by iterating over a folder (potentially recursively), 
-          # an equivalent output folder / file structure must be created dynamically. 
-          in_txt_path_single=$file_or_folder
-
-          # remove /veld/input/* so that only relevant subfolder stays
-          out_conllu_path_single=${in_txt_path_single#$in_txt_path}
-
-          # replace .txt with .conllu
-          out_conllu_path_single=${out_conllu_path_single/.txt/.conllu}
-
-          # concatenate main output folder passed via environment with dynamically created subpath
-          out_conllu_path_single="${out_conllu_path}${out_conllu_path_single}"
-
-          # step further down, to reuse the udpipe creation logic at the beginning of this
-          # function
-          do_udpipe_recursively "$in_txt_path_single" "$out_conllu_path_single"
-        # else
-
-          # is file, but not txt. Ignore then.
-          # echo "is neither folder, nor txt"
-        fi 
-
-      # if folder
-      elif [ -d "$file_or_folder" ]; then
-        # echo "is folder"
-
-        # recurse downwards into this subfolder
-        do_udpipe_recursively "$file_or_folder"
-      fi
+      do_udpipe_recursively "$file_or_folder"
     done
+
+  # if parameter is a non-existing path
   else
 
-    # if parameter is a non-existing path
-    echo "parameter is neither file, nor path. Exiting."
+    echo "file or folder does not exist: ${1}"
     exit 1
   fi
 }
 
 udpipe_args=$(build_args)
 
-do_udpipe_recursively "$in_txt_path" "$out_conllu_path"
+if [[ $in_txt_file = "" ]]; then
+  do_udpipe_recursively /veld/input/txt
+else
+  do_udpipe_recursively /veld/input/txt/"$in_txt_file"
+fi
 
